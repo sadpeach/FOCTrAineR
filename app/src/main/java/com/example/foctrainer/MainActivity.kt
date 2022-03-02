44ab75ccb9942e0be9259f3bf1536809f799e326
huang Jiayi <guangdongjiayi@gmail.com>
2022-03-02 20:31:29 +0800

    cameraX implementation

--- app/src/main/java/com/example/foctrainer/MainActivity.kt
+++ app/src/main/java/com/example/foctrainer/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.foctrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.foctrainer.databinding.ActivityExerciseBinding
+import com.example.foctrainer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun selectButtonOnClick(view: View){
+        val intent = Intent(this,Exercise::class.java)
+        startActivity(intent)
     }
 }
