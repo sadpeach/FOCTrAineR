61af2bce568432afcbf70d56ac5fbf1525b7fe88
Rachelinaa <Thatrachellow@gmail.com>
2022-03-17 09:30:26 +0800

    Updated home page

--- app/src/main/java/com/example/foctrainer/MainActivity.kt
+++ app/src/main/java/com/example/foctrainer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.foctrainer
 
+import android.app.PendingIntent.getActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -30,40 +31,45 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+import android.widget.TextView
+import com.google.android.material.navigation.NavigationView
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-//    private lateinit var lineValuesList: ArrayList<Entry>
-//    private lateinit var lineDataSet: LineDataSet
-//    private lateinit var dataLine: LineData
-//    private val exList = ArrayList<ExerciseModel>()
-//    private lateinit var exerciseAdapter: RecyclerAdapter
-//    var userID = 0
-////    var exerciseID = 0
-//    var exerciseName = ""
-//
-//    private val exerciseViewModel: ExerciseViewModel by viewModels {
-//        ExerciseViewModelFactory((application as FocTrainerApplication).exerciseRepository)
-//    }
-//
-//    private val userViewModel: UserViewModel by viewModels {
-//        UserViewModelFactory((application as FocTrainerApplication).userRepository)
-//    }
-//
-//    private val completeExerciseModel: CompletedExerciseViewModel by viewModels {
-//        CompletedExerciseViewModelFactory((application as FocTrainerApplication).completedExerciseRepository)
-//    }
+    private lateinit var lineValuesList: ArrayList<Entry>
+    private lateinit var lineDataSet: LineDataSet
+    private lateinit var dataLine: LineData
+    private val exList = ArrayList<ExerciseModel>()
+    private lateinit var exerciseAdapter: RecyclerAdapter
+    val homeFragment = HomeFragment()
+    val scheduleFragment = ScheduleFragment()
+
+    var userID = 0
+//    var exerciseID = 0
+    var exerciseName = ""
+
+    private val exerciseViewModel: ExerciseViewModel by viewModels {
+        ExerciseViewModelFactory((application as FocTrainerApplication).exerciseRepository)
+    }
+
+    private val userViewModel: UserViewModel by viewModels {
+        UserViewModelFactory((application as FocTrainerApplication).userRepository)
+    }
+
+    private val completeExerciseModel: CompletedExerciseViewModel by viewModels {
+        CompletedExerciseViewModelFactory((application as FocTrainerApplication).completedExerciseRepository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
 
-        val homeFragment = HomeFragment()
-        val scheduleFragment = ScheduleFragment()
         makeCurrentFragment(homeFragment)
-
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
         bottomNavView.setOnNavigationItemSelectedListener{ it ->
             when(it.itemId){
@@ -73,12 +79,12 @@ class MainActivity : AppCompatActivity() {
             true
         }
         //database testing: data insertion
-//        val user = UserModel(4,"jiayi","123",155.3f,45f,18.9f)
-//        userViewModel.createNewUser(user = user)
-//
-//        userViewModel.allUsers.observe(this, Observer{ users ->
-//            Log.d("db insertion testing", users[3].userName)
-//        })
+        val user = UserModel(4,"jiayi","123",155.3f,45f,18.9f)
+        userViewModel.createNewUser(user = user)
+
+        userViewModel.allUsers.observe(this, Observer{ users ->
+            Log.d("db insertion testing", users[3].userName)
+        })
 //
 //        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
 //        exerciseAdapter = RecyclerAdapter(exList)
@@ -87,38 +93,42 @@ class MainActivity : AppCompatActivity() {
 //        recyclerView.itemAnimator = DefaultItemAnimator()
 //        recyclerView.adapter = exerciseAdapter
 //
-//        displayUserDetails()
+        displayUserDetails()
 //        exerciseDetails()
-//        setLineChartData()
-//    }
-//
-//    fun selectButtonOnClick(view: View){
-//        val intent = Intent(this, Exercise::class.java)
-//        startActivity(intent)
-//    }
-//
-//    fun displayUserDetails(){
-//        val tvName = findViewById<TextView>(R.id.name)
-//        val textEditHeight = findViewById<EditText>(R.id.editHeight)
-//        val textEditWeight = findViewById<EditText>(R.id.editWeight)
-//        val tvBmi = findViewById<TextView>(R.id.bmi)
-//        tvName.text = ""
-//        tvBmi.text = ""
-//
-//        userViewModel.allUsers.observe(this, Observer<List<UserModel>>() { users ->
-//            userID = users[0].id
-//            tvName.append(users[0].userName)
-//            textEditHeight.setText(users[0].height.toString())
-//            textEditWeight.setText(users[0].weight.toString())
-//            tvBmi.append(users[0].bmi.toString())
-//        })
+        //setLineChartData()
     }
-
     private fun makeCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.wrapper, fragment)
             commit()
         }
+    fun selectButtonOnClick(view: View){
+        val intent = Intent(this, Exercise::class.java)
+        startActivity(intent)
+    }
+
+    fun displayUserDetails() {
+        val f1: FrameLayout? =
+            supportFragmentManager.findFragmentById(R.id.root_container) as FrameLayout?
+        val tvName = f1?.findViewById<TextView>(R.id.name)
+        val textEditHeight = f1?.findViewById<EditText>(R.id.editHeight)
+        val textEditWeight = f1?.findViewById<EditText>(R.id.editWeight)
+        val tvBmi = f1?.findViewById<TextView>(R.id.bmi)
+        Log.d("WHY", tvName.toString())
+
+        tvName?.text = ""
+        tvBmi?.text = ""
+
+        userViewModel.allUsers.observe(this, Observer<List<UserModel>>() { users ->
+            userID = users[0].id
+            tvName?.append(users[0].userName)
+            textEditHeight?.setText(users[0].height.toString())
+            textEditWeight?.setText(users[0].weight.toString())
+            tvBmi?.append(users[0].bmi.toString())
+        })
+    }
+
+
 //    fun exerciseDetails(){
         //Exercise
 //        val exlist = ArrayList<String>()
--- app/src/main/java/com/example/foctrainer/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foctrainer/fragments/HomeFragment.kt
@@ -5,21 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import com.example.foctrainer.R
+import com.example.foctrainer.RecyclerAdapter
+import com.example.foctrainer.databinding.ActivityMainBinding
+import com.example.foctrainer.databinding.FragmentHomeBinding
+import com.example.foctrainer.entity.ExerciseModel
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
+    lateinit var homeFragment: FragmentHomeBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private lateinit var tvName: EditText
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var lineValuesList: ArrayList<Entry>
+    private lateinit var lineDataSet: LineDataSet
+    private lateinit var dataLine: LineData
+    private val exList = ArrayList<ExerciseModel>()
+    private lateinit var exerciseAdapter: RecyclerAdapter
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+//        homeFragment = FragmentHomeBinding.inflate(layoutInflater)
+//        return homeFragment.root
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val tvName : TextView = view.findViewById(R.id.name)
+        val textEditHeight : EditText = view.findViewById(R.id.editHeight)
+        val textEditWeight : EditText = view.findViewById(R.id.editWeight)
+        val tvBmi : TextView = view.findViewById(R.id.bmi)
+        return view
     }
-//    fun displayUserDetails(){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+//    fun displayUserDetails(view: View){
 //        val tvName = findViewById<TextView>(R.id.name)
 //        val textEditHeight = findViewById<EditText>(R.id.editHeight)
 //        val textEditWeight = findViewById<EditText>(R.id.editWeight)
