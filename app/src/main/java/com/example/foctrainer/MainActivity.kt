af1b7d659c1f26b25e12d3a0435a852a8892b518
Rachelinaa <Thatrachellow@gmail.com>
2022-03-19 18:02:20 +0800

    Display List of Ex Categories, uncomment fragment

--- app/src/main/java/com/example/foctrainer/MainActivity.kt
+++ app/src/main/java/com/example/foctrainer/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.foctrainer
 
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +7,9 @@ import android.util.Log
 import android.view.View
 import com.example.foctrainer.exercise.Exercise
 import com.example.foctrainer.databinding.ActivityMainBinding
-import android.graphics.Color
 import android.widget.*
 import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foctrainer.databaseConfig.FocTrainerApplication
@@ -23,26 +19,24 @@ import com.example.foctrainer.entity.UserModel
 import com.example.foctrainer.fragments.HomeFragment
 import com.example.foctrainer.fragments.ScheduleFragment
 import com.example.foctrainer.viewModel.*
-import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.utils.ColorTemplate
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import android.widget.TextView
 import com.github.mikephil.charting.data.*
-import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var lineValuesList: ArrayList<Entry>
-    //Line Chart
-    private lateinit var lineDataSet: LineDataSet
-    private lateinit var dataLine: LineData
     private val exList = ArrayList<ExerciseModel>()
     private lateinit var exerciseAdapter: RecyclerAdapter
+
+    private var layoutManager : RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
+
     //BarChart
     private lateinit var myBarChart: BarChart
 
@@ -66,21 +60,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Navigation Bar
-        makeCurrentFragment(homeFragment)
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
-        bottomNavView.setOnNavigationItemSelectedListener{ it ->
-            when(it.itemId){
-                R.id.home -> makeCurrentFragment(homeFragment)
-                R.id.schedule -> makeCurrentFragment(scheduleFragment)
-            }
-            true
-        }
+//        setContentView(R.layout.activity_main)
+
+        //Navigation Bar -Fragment
+//        makeCurrentFragment(homeFragment)
+//        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+//        bottomNavView.setOnNavigationItemSelectedListener{ it ->
+//            when(it.itemId){
+//                R.id.home -> makeCurrentFragment(homeFragment)
+//                R.id.schedule -> makeCurrentFragment(scheduleFragment)
+//            }
+//            true
+//        }
 
         //database testing: data insertion
         val user = UserModel(4,"jiayi","123",155.3f,45f,18.9f)
@@ -94,17 +88,18 @@ class MainActivity : AppCompatActivity() {
 //            Log.d("Chart", ex.toString())
 //        })
 
-        val bundle = Bundle()
-        userViewModel.allUsers.observe(this, Observer<List<UserModel>>() { users ->
-            bundle.putString("name", "${users[0].userName}")
-            bundle.putString("height", "${users[0].height}")
-            bundle.putString("weight", "${users[0].weight}")
-            bundle.putString("bmi", "${users[0].bmi}")
-
-            val myObj = HomeFragment()
-            myObj.setArguments(bundle)
-            Log.d("obj", myObj.arguments.toString())
-        })
+//        val bundle = Bundle()
+//        userViewModel.allUsers.observe(this, Observer<List<UserModel>>() { users ->
+//            bundle.putString("name", "${users[0].userName}")
+//            bundle.putString("height", "${users[0].height}")
+//            bundle.putString("weight", "${users[0].weight}")
+//            bundle.putString("bmi", "${users[0].bmi}")
+//
+//            val myObj = HomeFragment()
+//            myObj.setArguments(bundle)
+//
+//            Log.d("obj", myObj.arguments.toString())
+//        })
 
 
 //        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
@@ -114,154 +109,137 @@ class MainActivity : AppCompatActivity() {
 //        recyclerView.itemAnimator = DefaultItemAnimator()
 //        recyclerView.adapter = exerciseAdapter
 
-//        displayUserDetails()
+        displayUserDetails()
 //        exerciseDetails()
-//        setLineChartData()
         //Barchart
-        //myBarChart = findViewById(R.id.BarChart)
-        //populateBarChart()
-    }
-    private fun makeCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.wrapper, fragment)
-            commit()
+        myBarChart = findViewById(R.id.BarChart)
+        populateBarChart()
+
+        val catlist = ArrayList<String>()
+         exerciseViewModel.allExercise.observe(this, Observer<List<ExerciseModel>>(){ exercise->
+            for (ex in exercise){
+                exerciseName = ex.name
+                catlist.add("$exerciseName")
+            }})
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = RecyclerAdapter(catlist)
         }
+
+    }
+
+    //Remove For Fragment
+//    private fun makeCurrentFragment(fragment: Fragment) =
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.wrapper, fragment)
+//            commit()
+//        }
     fun selectButtonOnClick(view: View){
         val intent = Intent(this, Exercise::class.java)
         startActivity(intent)
     }
 
     fun displayUserDetails() {
-        val f1: FrameLayout? =
-            supportFragmentManager.findFragmentById(R.id.root_container) as FrameLayout?
-        val tvName = f1?.findViewById<TextView>(R.id.name)
-        val textEditHeight = f1?.findViewById<EditText>(R.id.editHeight)
-        val textEditWeight = f1?.findViewById<EditText>(R.id.editWeight)
-        val tvBmi = f1?.findViewById<TextView>(R.id.bmi)
-        Log.d("WHY", tvName.toString())
+        val tvName = findViewById<TextView>(R.id.name)
+        val textEditHeight = findViewById<EditText>(R.id.editHeight)
+        val textEditWeight = findViewById<EditText>(R.id.editWeight)
+        val tvBmi = findViewById<TextView>(R.id.bmi)
 
         tvName?.text = ""
         tvBmi?.text = ""
 
         userViewModel.allUsers.observe(this, Observer<List<UserModel>>() { users ->
             userID = users[0].id
-            tvName?.append(users[0].userName)
-            textEditHeight?.setText(users[0].height.toString())
-            textEditWeight?.setText(users[0].weight.toString())
-            tvBmi?.append(users[0].bmi.toString())
+            tvName.append(users[0].userName)
+            textEditHeight.setText(users[0].height.toString())
+            textEditWeight.setText(users[0].weight.toString())
+            tvBmi.append(users[0].bmi.toString())
+            Log.d("WHY", users[0].toString())
         })
     }
 
 
-//    fun exerciseDetails(){
+    fun exerciseDetails(){
         //Exercise
-//        val exlist = ArrayList<String>()
-//        val exerciseListView = findViewById<ListView>(R.id.workoutCategoriesListView)
-//
-//        //get recycle view
-//        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//
-//        exerciseViewModel.allExercise.observe(this, Observer<List<ExerciseModel>>(){ exercise->
-//            for (ex in exercise){
-//                exerciseName = ex.name
-////                exerciseID = ex.id
-//                exlist.add("$exerciseName")
-//            }
-//            //attach the array adapter to the list view
+        val exlist = ArrayList<String>()
+        //val exerciseListView = findViewById<ListView>(R.id.workoutCategoriesListView)
+
+        //get recycle view
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        exerciseViewModel.allExercise.observe(this, Observer<List<ExerciseModel>>(){ exercise->
+            for (ex in exercise){
+                exerciseName = ex.name
+//                exerciseID = ex.id
+                exlist.add("$exerciseName")
+            }
+            //attach the array adapter to the list view
 //            exerciseListView.adapter = ArrayAdapter<String>(
 //                this, android.R.layout.simple_list_item_1,exlist)
-//
-////            recyclerView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, exlist)
-//        })
+
+//            recyclerView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, exlist)
+        })
 //        exerciseListView.setOnItemClickListener { adapterView, view, i, l ->
 //            Toast.makeText(this, "You have selected " + exlist[i], Toast.LENGTH_LONG).show()
 //        }
-//
-//        var totalCalories = 0f
-//        val tvTotalCalories = findViewById<TextView>(R.id.totalCalories)
-//        tvTotalCalories.text = ""
-//        completeExerciseModel.completedExercise.observe(this, Observer<List<CompletedExerciseModel>>(){ completedExercise->
-//            for ( comEx in completedExercise){
-//                val exUserId = comEx.userId
-//                if (exUserId == userID){
-//                    totalCalories += comEx.total_calories
-//                }
-//            }
-//            tvTotalCalories.append(totalCalories.toString())
-//        })
-//    }
 
-    //line Chart
-//    fun setLineChartData(){
-//        val lineChart = findViewById<LineChart>(R.id.lineChart)
-//        lineValuesList = ArrayList();
-//        completeExerciseModel.completedExercise.observe(this, Observer<List<CompletedExerciseModel>>(){ completedExercise->
-//            for (eachEx in completedExercise){
-//                val comExUserId = eachEx.userId
-//                val comExExerciseId = eachEx.exerciseId
-//                val calories = eachEx.total_calories
-//                if (comExUserId == userID){
-//                    lineValuesList.add(Entry(comExExerciseId.toFloat(), calories))
-//                }
-//            }
-//            lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM)
-//            lineChart.axisRight.isEnabled = false
-//            lineDataSet = LineDataSet(lineValuesList, "WorkOut")
-//            dataLine = LineData(lineDataSet)
-//            lineChart.data = dataLine
-//
-//            lineDataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
-//            lineDataSet.valueTextColor = Color.BLACK
-//            lineDataSet.valueTextSize = 15f
-//            lineDataSet.setDrawFilled(true)
-//
-//            lineChart.setBackgroundColor(resources.getColor(R.color.white))
-//            lineChart.animateXY(2000,2000)
-//        })
-//
-//    }
+        var totalCalories = 0f
+        val tvTotalCalories = findViewById<TextView>(R.id.totalCalories)
+        tvTotalCalories.text = ""
+        completeExerciseModel.completedExercise.observe(this, Observer<List<CompletedExerciseModel>>(){ completedExercise->
+            for ( comEx in completedExercise){
+                val exUserId = comEx.userId
+                if (exUserId == userID){
+                    totalCalories += comEx.total_calories
+                }
+            }
+            tvTotalCalories.append(totalCalories.toString())
+        })
+    }
+
 
     //Barchart
-//    fun populateBarChart() {
-//        //adding values
-//        val myBarEntries: ArrayList<BarEntry> = ArrayList()
-//        var i = 0
-//
-//        completeExerciseModel.chartSummary.observe(this, Observer<List<String>>() { completedExercise ->
-//            for (eachEx in completedExercise) {
-//                Log.d("PLS", eachEx.toString())
-//                val comExUserId = eachEx.userId
-//                val comExExerciseId = eachEx.exerciseId
-//                val calories = eachEx.total_calories
-//                if (comExUserId == userID) {
-//                    myBarEntries.add(BarEntry(comExExerciseId.toFloat(), calories))
-//                }
-//            }
-//
-//            val barDataSet = BarDataSet(myBarEntries, "Work Out Summary")
-//            //set a template coloring
-//            barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-//            val data = BarData(barDataSet)
-//            myBarChart.data = data
-//            //setting the x-axis
-//            val xAxis: XAxis = myBarChart.xAxis
-//            //calling methods to hide x-axis gridlines
-//            myBarChart.axisLeft.setDrawGridLines(false)
-//            xAxis.setDrawGridLines(false)
-//            //xAxis.setDrawAxisLine(false)
-//            myBarChart.setBackgroundColor(resources.getColor(R.color.white))
-//            //remove legend
-//            myBarChart.legend.isEnabled = true
-//
-//            //remove description label
-//            myBarChart.description.isEnabled = false
-//
-//            //add animation
-//            myBarChart.animateY(3000)
-//            //refresh the chart
-//            myBarChart.invalidate()
-//        })
-//    }
+    fun populateBarChart() {
+        //adding values
+        val myBarEntries: ArrayList<BarEntry> = ArrayList()
+        var i = 0
+        val xAxis: ArrayList<String> = ArrayList()
+        completeExerciseModel.completedExercise.observe(this, Observer<List<CompletedExerciseModel>>() { completedExercise ->
+            for (eachEx in completedExercise) {
+                Log.d("PLS", eachEx.toString())
+                val comExUserId = eachEx.userId
+                val comExExerciseId = eachEx.exerciseId
+                val calories = eachEx.total_calories
+                if (comExUserId == userID) {
+                    myBarEntries.add(BarEntry(comExExerciseId.toFloat(), calories))
+                }
+            }
 
+            val barDataSet = BarDataSet(myBarEntries, "Work Out Summary")
+            //set a template coloring
+            barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+            val data = BarData(barDataSet)
+            myBarChart.data = data
+            //setting the x-axis
+            val xAxis: XAxis = myBarChart.xAxis
+            //calling methods to hide x-axis gridlines
+            myBarChart.axisLeft.setDrawGridLines(false)
+            xAxis.setDrawGridLines(false)
+            //xAxis.setDrawAxisLine(false)
+            myBarChart.setBackgroundColor(resources.getColor(R.color.white))
+            //remove legend
+            myBarChart.legend.isEnabled = true
+
+            //remove description label
+            myBarChart.description.isEnabled = false
+
+            //add animation
+            myBarChart.animateY(3000)
+            //refresh the chart
+            myBarChart.invalidate()
+        })
+    }
 }
--- app/src/main/java/com/example/foctrainer/RecyclerAdapter.kt
+++ app/src/main/java/com/example/foctrainer/RecyclerAdapter.kt
@@ -1,29 +1,40 @@
 package com.example.foctrainer
 
-import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.foctrainer.entity.ExerciseModel
-import com.example.foctrainer.mapper.ExerciseMapper
+import com.example.foctrainer.exercise.Exercise
 
-class RecyclerAdapter(private val exerciseList: List<ExerciseModel>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(private val exerciseList: ArrayList<String>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("IDK", exerciseList[position].toString())
         val ex = exerciseList[position]
-        holder.exerciseCategories.text = ex.name
+        holder.textExCategories.text = ex.toString()
+
+        holder.itemView.setOnClickListener { v: View ->
+            Log.d("Click", "U have click")
+            v.context.startActivity(Intent(v.context, Exercise::class.java))
+            Toast.makeText(v.context,"Click", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun getItemCount(): Int {
         return exerciseList.size
     }
 
-    class ViewHolder (view : View): RecyclerView.ViewHolder(view){
-        val exerciseCategories : TextView = view.findViewById(R.id.tvExerciseCat)
+    inner class ViewHolder (view : View): RecyclerView.ViewHolder(view){
+        var textExCategories : TextView
+        init {
+            textExCategories = view.findViewById(R.id.tvExerciseCat)
+        }
     }
 }
